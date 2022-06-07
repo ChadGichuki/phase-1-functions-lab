d1426cf0132ead648509af062bf41331fe1c10bb
richard gichuki <chadgichuki@gmail.com>
2022-06-07 14:58:05 +0300

    Completed all the functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+    if (street > 42) {
+        return street - 42;
+    } return 42 - street;
+}
+
+function distanceFromHqInFeet(street) {
+    let blocks = distanceFromHqInBlocks(street);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, finish) {
+    if (finish > start) {
+        return((finish - start) * 264);
+    } return((start - finish) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <=400 ) {
+        return 0;
+    } else if (distance >400 && distance <=2000) {
+        return ((distance - 400) * 0.02);
+    } else if (distance >2000 && distance <=2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
